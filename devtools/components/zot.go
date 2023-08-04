08abe7e7b8a0166e35ebce61fb722717b732d02b
Aaron Siddhartha Mondal <aaron@eomii.org>
2023-08-04 17:41:56 +0200

    ✨ Migrate to pulumi-kubernetes v4 (#192)

--- devtools/components/zot.go
+++ devtools/components/zot.go
@@ -3,7 +3,7 @@ package components
 import (
 	"fmt"
 
-	helmv3 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/helm/v3"
+	helmv3 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/helm/v3"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
